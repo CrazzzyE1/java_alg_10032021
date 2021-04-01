9813b09493a8ab1bda73ce9fb082e84c313a8925
litvakivan <litvakivan@gmail.com>
2021-04-01 12:46:09 +0300

    Lesson 7

--- src/Lesson7/BreadthFirstPath.java
+++ src/Lesson7/BreadthFirstPath.java
@@ -2,15 +2,10 @@ package Lesson7;
 
 import java.util.LinkedList;
 
-public class BreadthFirstPath {
-    private boolean[] marked;
-    private int[] edgeTo;
-    private int source;
+public class BreadthFirstPath extends FirstPath {
 
     public BreadthFirstPath(Graph g, int source) {
-        this.source = source;
-        marked = new boolean[g.getVertexCount()];
-        edgeTo = new int[g.getVertexCount()];
+        super(g, source);
         bfs(g, source);
     }
 
@@ -18,32 +13,15 @@ public class BreadthFirstPath {
         LinkedList<Integer> queue = new LinkedList<>();
         queue.addLast(source);
         marked[source] = true;
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int vertex = queue.removeFirst();
             for (int w : g.getAdjList(vertex)) {
-                if(!marked[w]){
+                if (!marked[w]) {
                     marked[w] = true;
                     edgeTo[w] = vertex;
                     queue.addLast(w);
                 }
             }
         }
-    }
-
-    public boolean hasPathTo(int w) {
-        return marked[w];
-    }
-
-    public LinkedList<Integer> pathTo(int w) {
-        if (!hasPathTo(w)) {
-            return null;
-        }
-        LinkedList<Integer> stack = new LinkedList<>();
-        int vertex = w;
-        while (vertex != source) {
-            stack.push(vertex);
-            vertex = edgeTo[vertex];
-        }
-        return stack;
     }
 }
--- src/Lesson7/Main.java
+++ src/Lesson7/Main.java
@@ -1,5 +1,7 @@
 package Lesson7;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,10 +10,27 @@ public class Main {
 //        Задать ребра и найти кратчайший путь с помощью поиска в ширину.
 //        2*. Вынести в родительский класс общую часть классов поиска в глубину и ширину.
 
-
-
-
-
+   //Task 1
+        Graph graph = new Graph(10);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(0, 5);
+        graph.addEdge(0, 8);
+        graph.addEdge(3, 4);
+        graph.addEdge(2, 6);
+        graph.addEdge(5, 9);
+        graph.addEdge(6, 8);
+        graph.addEdge(1, 7);
+        graph.addEdge(7, 8);
+        BreadthFirstPath bf = new BreadthFirstPath(graph, 0);
+        System.out.println(bf.hasPathTo(6));
+        System.out.println(bf.pathTo(6));
+        System.out.println(bf.hasPathTo(9));
+        System.out.println(bf.pathTo(9));
+        bf = new BreadthFirstPath(graph, 7);
+        System.out.println(bf.hasPathTo(9));
+        System.out.println(bf.pathTo(9));
 
 //        Graph graph = new Graph(10);
 //        graph.addEdge(1,2);
@@ -27,11 +46,10 @@ public class Main {
 //        DepthFirstPath dfp = new DepthFirstPath(graph, 0);
 //        System.out.println(dfp.hasPathTo(2));
 //        System.out.println(dfp.pathTo(2));
-
+//
 //        BreadthFirstPath bfp  = new BreadthFirstPath(graph, 0);
 //        System.out.println(bfp.hasPathTo(2));
 //        System.out.println(bfp.pathTo(2));
-
 
 
     }
